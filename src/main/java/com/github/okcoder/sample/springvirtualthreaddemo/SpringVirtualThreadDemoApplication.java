b4f8a85fed6a0b4ede72cdbcaa319fd0009e5e51
okcoder <okcoder@live.com>
2023-11-13 22:54:02 +0900

    create project

---
+++ src/main/java/com/github/okcoder/sample/springvirtualthreaddemo/SpringVirtualThreadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.okcoder.sample.springvirtualthreaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVirtualThreadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVirtualThreadDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/okcoder/sample/springvirtualthreaddemo/SpringVirtualThreadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.okcoder.sample.springvirtualthreaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVirtualThreadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
